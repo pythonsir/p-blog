1e760b0d9cfec02a166d729c4d9a792638e289c1
python <baidu211@vip.qq.com>
2018-09-09 23:08:31 +0800

    sit init
    sit init

---
+++ wp-content/themes/python/js/pmain.js
@@ -0,0 +1,24 @@
+new Vue({
+    el: '#app',
+    data(){
+        return {
+            skills:[
+                {
+                    title:"职业",
+                    info:"全栈工程师、热爱编程、技能全栈、有代码洁癖。"
+                },
+                {
+                    title:"坐标",
+                    info:"山西太原"
+                },
+                {
+                    title:'GitHub地址',
+                    info:"https://github.com/pythonsir",
+                    url:"https://github.com/pythonsir"
+                }
+            ]
+        }
+    }
+
+
+})
