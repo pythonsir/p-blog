5384acafc5ec459ba907d1fa98ddfdc37cffc242
python <baidu211@vip.qq.com>
2018-10-09 22:52:51 +0800

    修复部分bug

--- wp-content/themes/python/template-parts/toolbars.php
+++ wp-content/themes/python/template-parts/toolbars.php
@@ -11,7 +11,7 @@
 <div class="Header">
     <div class="Page">
         <h1 class="Logo"><a href="https://colorlib.cn/">Logo</a></h1>
-        <? wp_nav_menu(array('container' => '','menu_class' => 'Navigates fl','theme_location' => 'menu-main')) ?>
+        <?php wp_nav_menu(array('container' => '','menu_class' => 'Navigates fl','theme_location' => 'menu-main')) ?>
 
     </div>
 </div>
